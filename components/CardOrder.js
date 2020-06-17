c52573f1df619a6ddde1aed42de230a530960090
Adam-SBS <adam.spagnuolo@soprabanking.com>
2020-06-17 18:55:58 +0200

    retry motion with fix scroll&drag

--- components/CardOrder.js
+++ components/CardOrder.js
@@ -1,7 +1,28 @@
+import { useDrag } from 'react-use-gesture'
+import { motion, useAnimation } from "framer-motion"
+import React, { useState, useEffect } from 'react';
+
 export default function CardOrder(props) {
 
+    const [allowScroll, setAllowScroll] = useState(false)
+    useEffect(() => {
+        if (allowScroll) {
+        const handleTouch = event => {
+            event.stopPropagation()
+        }
+        document.documentElement.addEventListener('touchmove', handleTouch)
+        return () => {
+            document.documentElement.removeEventListener('touchmove', handleTouch)
+        }
+        }
+    }, [allowScroll])
+
+    function onDrag(event, info) {
+        console.log(info.point.x)
+    }
+
     return (
-        <div className="card-order p-4 mb-8 rounded shadow-md flex flex-row relative" >
+        <motion.div className="card-order p-4 mb-8 rounded shadow-md flex flex-row relative" drag="x" dragConstraints={{ left: 0, right: 0 }} onDrag={onDrag} onDragStart={(event, info) => {setAllowScroll(Math.abs(info.delta.y) > Math.abs(info.delta.x))}}>
             <div className="bulle-container absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
                 <div className="bulle relative">
                     <div className="qty-container w-5 h-5 rounded-full text-center relative">
@@ -25,6 +46,6 @@ export default function CardOrder(props) {
                     left: 50%;
                 }
             `}</style>
-        </div>
+        </motion.div>
     )
 }
