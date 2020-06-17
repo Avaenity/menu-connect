c71d815a849695801170d4eafac5a8c16f554dac
Adam-SBS <adam.spagnuolo@soprabanking.com>
2020-06-17 17:29:45 +0200

    test with react-use-gesture

--- components/CardOrder.js
+++ components/CardOrder.js
@@ -1,9 +1,30 @@
-import { motion } from "framer-motion"
+import { useDrag } from 'react-use-gesture'
+import { motion, useAnimation } from "framer-motion"
 
 export default function CardOrder(props) {
 
+    const animation = useAnimation({
+        x: 0,
+        transition: {
+            type: "spring",
+            stiffness: 1,
+        },
+    });
+    const bind = useDrag(
+        (state) => {
+        if (state.dragging) {
+            animation.start({ x: state.movement[0] });
+        } else {
+            animation.start({ x: 0 });
+        }
+        },
+        {
+        axis: "x",
+        }
+    );
+
     return (
-        <motion.div className="card-order p-4 mb-8 rounded shadow-md flex flex-row relative" drag="x" dragConstraints={{ left: 0, right: 0 }} dragElastic={0.2} dragDirectionLock onDrag={(event, info) => console.log(info.point.x, info.point.y)}>
+        <motion.div className="card-order p-4 mb-8 rounded shadow-md flex flex-row relative" {...bind()} animate={animation}>
             <div className="bulle-container absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
                 <div className="bulle relative">
                     <div className="qty-container w-5 h-5 rounded-full text-center relative">
