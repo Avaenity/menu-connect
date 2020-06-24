401a7b0bb3c8c27e934b5c833f19c760308b55bc
Adam-SBS <adam.spagnuolo@soprabanking.com>
2020-06-24 22:09:30 +0200

    fix warning on removing DOM element with JS. Use class hidden

--- pages/_app.js
+++ pages/_app.js
@@ -80,7 +80,7 @@ function MyApp({ Component, pageProps }) {
 
 	useEffect(() => {
 		setTimeout(function(){document.getElementById("splash-screen").style.transform = "translateX(-100%)"; }, 4000);
-		setTimeout(function(){ document.getElementById("splash-screen").parentNode.removeChild(document.getElementById("splash-screen")) }, 5000);
+		setTimeout(function(){ document.getElementById("splash-screen").classList.add("hidden")}, 5000);
 	}, []);
 
 
